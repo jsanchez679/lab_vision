2265da75b23e4cb2d90665063353c0fc5c457de7
Juliana Sánchez Posada <j.sanchez679@uniandes.edu.co>
2015-03-18 22:36:16 -0500

    lab4_segmentation upload

--- lab4_segmentation/images/prueba.m
+++ lab4_segmentation/images/prueba.m
@@ -1,28 +1,19 @@
+
 clc
-im=imread('2018.jpg');
+close all 
 
-lwd=size(im);
-matrix_out=zeros(lwd(1)*lwd(2),5);
+im=imread('6046.jpg');
+my_segmentation = segment_by_clustering(im,'rgb','k-means',1,10);
 
-x=1:1:lwd(2);
-y=1:1:lwd(1);
+% 
+% figure()
+% subplot(2,1,1)
+% image(im)
+% subplot(2,1,2)
+% image(my_segmentation)
+% colormap (colorcube);
 
-matrix_x=zeros(lwd(1),lwd(2));
-matrix_y=zeros(lwd(1),lwd(2));
 
-for i=1:lwd(1)
-    matrix_x(i,:)=x;
-end
 
-for i=1:lwd(2)
-    matrix_y(:,i)=y;
-end
 
-for i=1:5
-    if i<4
-        matrix_out(:,i)=reshape(im(:,:,i),lwd(1)*lwd(2),1);
-    else 
-        matrix_out(:,4)=reshape(matrix_x,lwd(1)*lwd(2),1);
-        matrix_out(:,5)=reshape(matrix_y,lwd(1)*lwd(2),1);
-    end
-end
+
